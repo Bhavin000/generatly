6fd87e5fd73fcf0a48de88ae697079900f8b9af1
bhavin <bhavinsojitra000@gmail.com>
2021-09-25 16:18:45 +0530

    add tests for starter and page

---
+++ bricks/page/__brick__/test/src/presentation/pages/{{#snakeCase}}{{page_name}}{{/snakeCase}}_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('{{#pascalCase}}{{page_name}}{{/pascalCase}}', () {
+    test('', () {});
+    testWidgets('', (WidgetTester widgetTester) async {});
+  });
+}
