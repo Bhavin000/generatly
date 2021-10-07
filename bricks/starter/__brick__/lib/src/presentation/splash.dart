3bf8076e13e50ea7c5378d4f2267836dffefa1bc
bhavin <bhavinsojitra000@gmail.com>
2021-10-07 11:00:51 +0530

    update splash and pubspec

--- bricks/starter/__brick__/lib/src/presentation/splash.dart
+++ bricks/starter/__brick__/lib/src/presentation/splash.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:{{#snakeCase}}{{app_name}}{{/snakeCase}}/src/presentation/routes/route_definations.dart';
 
 class Splash extends StatelessWidget {
   const Splash({Key? key}) : super(key: key);
-  
+
   goToHomePage(context) async {
     await Future.delayed(const Duration(milliseconds: 500));
     Navigator.pushReplacementNamed(context, Routes.homePage);
