1278b82f0c8c58c4b507fd7874304ded28b6f322
bhavin <bhavinsojitra000@gmail.com>
2021-10-07 10:35:50 +0530

    update bricks

--- bricks/cubit/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit/{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart
+++ bricks/cubit/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit/{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart
@@ -3,7 +3,7 @@ part of '{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart';
 @immutable
 abstract class {{#pascalCase}}{{name}}{{/pascalCase}}State extends Equatable {
   @override
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [];
 }
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Initial extends {{#pascalCase}}{{name}}{{/pascalCase}}State {}
--- bricks/starter/__brick__/lib/src/presentation/splash.dart
+++ bricks/starter/__brick__/lib/src/presentation/splash.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class Splash extends StatelessWidget {
   const Splash({Key? key}) : super(key: key);
+  
+  goToHomePage(context) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    Navigator.pushReplacementNamed(context, Routes.homePage);
+  }
 
   @override
   Widget build(BuildContext context) {
+    goToHomePage(context);
     return const Scaffold(
       body: Center(
         child: Text('Splash'),
